b33f3c8986185ced27137e8134e7c7a9a0d7b1bf
Óscar Beiro <oscar@ticgal.com>
2026-08-18 10:09:32 +0200

    Replace sha1 with sha256 in DinahostingDriver's contentHash
    Silences PHPStan's insecure-crypto-function rule; contentHash() was
    already documented as a non-cryptographic disambiguation fingerprint,
    not a security use, but sha1 tripped the blanket check anyway.

--- src/Driver/DinahostingDriver.php
+++ src/Driver/DinahostingDriver.php
@@ -749,7 +749,7 @@ class DinahostingDriver extends AbstractDriver implements RegistrarDriverInterfa
      */
     private static function contentHash(string $type, string $data): string
     {
-        return substr(sha1($data), 0, 16);
+        return substr(hash('sha256', $data), 0, 16);
     }
 
     /**
